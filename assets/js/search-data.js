5a3308468d2ee4096868458a8dc065aec8caf115
wallacehuich <161118203+wallacehuich@users.noreply.github.com>
2025-09-02 17:59:41 +0000

    Deploying to gh-pages from @ wallacehuich/wallacehuich.github.io@67cbdcb1857bc5db4c38be3baf321123f35b55c3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,8 +23,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-teaching-and-outreach",
-          title: "Teaching and Outreach",
+        },{id: "nav-teaching-amp-outreach",
+          title: "Teaching &amp; Outreach",
           description: "",
           section: "Navigation",
           handler: () => {
